094caa080a6b92032aa23cd2fc0f48bf828faffd
Juan Torres <juanto121@hotmail.com>
2014-08-20 17:53:22 -0500

    Initial commit, remote horizontal page sliding

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express')
+  , app = express()  
+  , server = require('http').createServer(app)
+  , path = require('path')
+  , io = require('socket.io').listen(server)
+  , spawn = require('child_process').spawn
+  , omx = require('omxcontrol')
+  , pirateBay = require('thepiratebay');
+
+app.listen(process.env.TEST_PORT || 80);
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/views')));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/views/index.html');
+});
+
+app.get('/remote', function (req, res) {
+  res.sendfile(__dirname + '/views/remote.html');
+});
+
---
+++ public/js/remote.js
@@ -0,0 +1,64 @@
+
+remote.currentSection = 0;
+remote.totalSections  = function(){
+	return remote.sections.length;
+}
+
+remote.sections = [
+
+	{
+		id_section : 0
+	},
+
+	{
+		id_section : 1
+	}
+
+];
+
+remote.loadSection = function(){
+	var section = remote.sections[remote.currentSection];
+	$(remote.getSection(remote.currentSection)).show();
+}
+
+remote.getSection = function(id){
+	return $('.section[data-section='+id+']');
+}
+
+remote.init = function(){
+	return remote.loadSection();
+}
+
+remote.next = function(){
+	var nextSection, pageWidth, section;
+	nextSection = (remote.currentSection + 1)%2;
+	pageWidth = $(window).width();
+	slideInNext = function(){
+		var followingSection;
+		followingSection = remote.getSection(nextSection);
+		$(followingSection).css({left:pageWidth}).show();
+		remote.currentSection = nextSection;
+		return $(followingSection).animate({
+			left : 0
+		}, 300,function(){
+			console.log('slidein_nextSection');
+		});
+	};
+	section = remote.getSection(remote.currentSection);
+	return $(section).animate({
+		left: 0-pageWidth
+	}, 300,  function(){
+		$(this).hide();
+		return slideInNext();
+	});
+}
+
+
+$(function(){
+
+	remote.init();
+	$('#next').click(function(){
+		return remote.next();
+	});
+
+});
